f889085630686a8da3548950efac8188e2826eda
KAispread <kai@igiuui-MacBookPro.local>
2023-09-02 19:18:09 +0900

    Initial commit

---
+++ src/main/kotlin/lec01.kt
@@ -0,0 +1,26 @@
+
+fun main() {
+    // variable in Kotlin
+    var number1 = 10
+    val number4: Long = 10L
+
+    // val collection can add elements
+    val array = ArrayList<Int>()
+    array.add(10)
+    array.add(10)
+
+    // NULLABLE
+    var nullabe = 1
+    // nullabe = null -> error
+    var number2: Long? = 10L
+    number2 = null
+
+    var person = Person("John", 20)
+
+}
+
+class Person(val name: String, val age: Int) {
+    fun isAdult(): Boolean {
+        return age >= 20
+    }
+}
